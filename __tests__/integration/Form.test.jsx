d788e19958bd7d0aa8cb3f38a2dc63cdf7c908e6
Lukita <lucmaieski@gmail.com>
2022-10-03 16:08:30 -0300

    Create form required field error message integration tests

--- __tests__/integration/Form.test.jsx
+++ __tests__/integration/Form.test.jsx
@@ -1,6 +1,6 @@
 import Form from "../../src/components/form/Form";
 import "@testing-library/jest-dom";
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
 describe("Test form component interactions", () => {
@@ -8,22 +8,43 @@ describe("Test form component interactions", () => {
     render(<Form />);
   });
 
-  it('must have a required field error message with empty inputs', async () => {
-        const nameInput = screen.getByPlaceholderText('Nome');
-        const emailInput = screen.getByPlaceholderText('E-mail');
-        const passwordInput = screen.getByPlaceholderText('Senha');
+  it("must have a required field error message with empty inputs", () => {
+    const nameInput = screen.getByPlaceholderText("Nome");
+    const emailInput = screen.getByPlaceholderText("E-mail");
+    const passwordInput = screen.getByPlaceholderText("Senha");
 
-        expect(nameInput).toHaveValue('')
-        expect(emailInput).toBeEmptyDOMElement();
-        expect(passwordInput).toBeEmptyDOMElement();
+    expect(nameInput).toHaveValue("");
+    expect(emailInput).toBeEmptyDOMElement();
+    expect(passwordInput).toBeEmptyDOMElement();
 
+    const nameRequiredError = screen.getByText(/Name is required/i);
+    const emailRequiredError = screen.getByText(/E-mail is required/i);
+    const passwordRequiredError = screen.getByText(/Password is required/i);
 
-        const nameRequiredError = screen.getByText(/Name is required/i);
-        const emailRequiredError = screen.getByText(/E-mail is required/i);
-        const passwordRequiredError = screen.getByText(/Password is required/i);
+    expect(nameRequiredError).toBeInTheDocument();
+    expect(emailRequiredError).toBeInTheDocument();
+    expect(passwordRequiredError).toBeInTheDocument();
+  });
 
-        expect(nameRequiredError).toBeInTheDocument();
-        expect(emailRequiredError).toBeInTheDocument();
-        expect(passwordRequiredError).toBeInTheDocument();
-  })
+  it("required field error message should not be in the screen with valid input values", async () => {
+    const nameInput = screen.getByPlaceholderText("Nome");
+    const emailInput = screen.getByPlaceholderText("E-mail");
+    const passwordInput = screen.getByPlaceholderText("Senha");
+
+    const nameRequiredError = screen.getByText(/Name is required/i);
+    const emailRequiredError = screen.getByText(/E-mail is required/i);
+    const passwordRequiredError = screen.getByText(/Password is required/i);
+
+    expect(nameRequiredError).toBeInTheDocument();
+    expect(emailRequiredError).toBeInTheDocument();
+    expect(passwordRequiredError).toBeInTheDocument();
+
+    await userEvent.type(nameInput, "Lucas");
+    await userEvent.type(emailInput, "lucmaieski@gmail.com");
+    await userEvent.type(passwordInput, "123456789");
+
+    expect(nameRequiredError).not.toBeInTheDocument();
+    expect(emailRequiredError).not.toBeInTheDocument();
+    expect(passwordRequiredError).not.toBeInTheDocument();
+  });
 });
