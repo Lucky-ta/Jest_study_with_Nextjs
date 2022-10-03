3a06f96a1fab4bde7ed7380b9c49e012cb893441
Lukita <lucmaieski@gmail.com>
2022-10-03 15:23:13 -0300

    Create warningValidation component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,19 +1,70 @@
-import React from "react";
-import * as FormComponents from './index'
-import * as InputComponents from '../Inputs/index'
-import * as ButtonComponents from '../buttons/index'
+import React, { useState } from "react";
+import * as FormComponents from "./index";
+import * as InputComponents from "../Inputs/index";
+import * as ButtonComponents from "../buttons/index";
+import WarningValidation from "../warningValidation/warningValidation";
 
 function Form() {
-    return(
-        <FormComponents.FormContainer>
-            <FormComponents.FormInputsContainer>
-                <InputComponents.BasicInput type="text" placeholder="Nome" />
-                <InputComponents.BasicInput type="text" placeholder="E-mail"/>
-                <InputComponents.BasicInput type="text" placeholder="Senha"/>
-            </FormComponents.FormInputsContainer>
-            <ButtonComponents.BasicButton>Entrar</ButtonComponents.BasicButton>
-        </FormComponents.FormContainer>
-    )
+  const [formInputs, setFormInputs] = useState([]);
+
+  const handleFormInputValues = ({ target }) => {
+    setFormInputs({
+        ...formInputs,
+        [target.id]: target.value
+    })
+
+    console.log(formInputs)
+
+  };
+
+  const isButtonActive = () => {
+    if (!formInputs.name) return true;
+    if (!formInputs.email) return true;
+    if (!formInputs.password) return true;
+    return false
+  };
+
+  const handleButton = () => {
+    console.log('Enviado!');
+  }
+
+  return (
+    <FormComponents.FormContainer>
+      <FormComponents.FormInputsContainer>
+        <InputComponents.BasicInput
+          onChange={(e) => handleFormInputValues(e)}
+          type="text"
+          id="name"
+          placeholder="Nome"
+        />
+        { !formInputs.name && <WarningValidation fieldName="Name"/>}
+
+        <InputComponents.BasicInput
+          onChange={(e) => handleFormInputValues(e)}
+          type="text"
+          id="email"
+          placeholder="E-mail"
+        />
+        { !formInputs.email && <WarningValidation fieldName="E-mail"/>}
+
+        <InputComponents.BasicInput
+          onChange={(e) => handleFormInputValues(e)}
+          type="text"
+          id="password"
+          placeholder="Senha"
+        />
+        { !formInputs.password && <WarningValidation fieldName="Password"/>}
+
+      </FormComponents.FormInputsContainer>
+      <ButtonComponents.BasicButton
+        type="button"
+        disabled={isButtonActive()}
+        onClick={handleButton}
+      >
+        Entrar
+      </ButtonComponents.BasicButton>
+    </FormComponents.FormContainer>
+  );
 }
 
 export default Form;
---
+++ src/components/warningValidation/warningValidation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WarningValidation({ fieldName }) {
+    return <h1>{`${fieldName} is required`}</h1>
+};
+
+export default WarningValidation;
